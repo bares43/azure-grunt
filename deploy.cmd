5f6f9233d66beb669bd0f28b91eec4d9fb2e3884
Jan Bareš <janbares43@gmail.com>
2017-09-28 14:22:40 +0200

    deployment script

--- deploy.cmd
+++ deploy.cmd
@@ -95,7 +95,7 @@ IF !ERRORLEVEL! NEQ 0 goto error
    echo pred cd
    cd "%DEPLOYMENT_TARGET%"
    echo pred gruntem
-   call "./node_modules/.bin/grunt build"
+   call grunt build
    IF !ERRORLEVEL! NEQ 0 goto error
    echo po gruntu
    cd - > /dev/null
