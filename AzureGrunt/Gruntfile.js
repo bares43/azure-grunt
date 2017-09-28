8ecd84916b5a010125dd8dcf82b09cfa688baf26
Jan Bareš <janbares43@gmail.com>
2017-09-28 13:29:17 +0200

    init

---
+++ AzureGrunt/Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'Content/Sass/',
+          src: ['*.scss'],
+          dest: 'Content/',
+          ext: '.css'
+        }]
+      }
+    },
+    watch: {
+      css: {
+        files: ['Content/Sass/*'],
+        tasks: ['build_css']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build_css', ['sass']);
+  grunt.registerTask('build', ['build_css']);
+  grunt.registerTask('default', ['watch']);
+};
